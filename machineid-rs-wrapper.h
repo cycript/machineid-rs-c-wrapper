46067ec1a1113c8873518675635acb4e9dee563a
cycript <84468564+cycript@users.noreply.github.com>
2024-04-30 20:47:47 +0530

    add C-style header

---
+++ machineid-rs-wrapper.h
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum CComponentCode_Tag {
+  SystemID,
+  OSName,
+  CPUCores,
+  CPUID,
+  DriveSerial,
+  MacAddress,
+  FileToken,
+  Username,
+  MachineName,
+} CComponentCode_Tag;
+
+typedef struct CComponentCode {
+  CComponentCode_Tag tag;
+  union {
+    struct {
+      const char *file_token;
+    };
+  };
+} CComponentCode;
+
+void setup_builder(const struct CComponentCode *component_ptr, uintptr_t len);
+
+char *get_machine_id(const uint8_t *secret_key);
